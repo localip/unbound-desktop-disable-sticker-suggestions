6125a163af92c4c595ec53e134b94606847988b2
eternal <eternal.git@gmail.com>
2022-07-07 11:21:19 +0100

    rewrite

---
+++ src/index.tsx
@@ -0,0 +1,31 @@
+import Plugin from '@entities/plugin';
+
+import { findByProps } from '@webpack';
+
+const [
+   Expressions,
+   Stickers
+] = findByProps(
+   'useStickerSuggestionResults',
+   'queryStickers',
+   { bulk: true }
+);
+
+export default class extends Plugin {
+   public style: {
+      remove: () => void;
+   };
+
+   start() {
+      this.patcher.after(Stickers, 'queryStickers', (_, [, , isChat], res) => {
+         if (isChat) return [];
+      });
+
+      this.patcher.instead(Expressions, 'useStickerSuggestionResults', () => []);
+   }
+
+   stop() {
+      this.style.remove();
+      this.patcher.unpatchAll();
+   }
+}
